871e1bdd29d85558daf55d0f8205f0c5087f02a5
vu <truonganhvu205@gmail.com>
2024-02-02 10:51:18 +0700

    commit

--- authentication_system/settings.py
+++ authentication_system/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()  # loads the configs from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
